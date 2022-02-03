c77b69c0658ed5d335abe6e14a3cf7df58afea83
darya <darya2000a0@gmail.com>
2022-02-03 11:57:18 +0330

    Initial commit

---
+++ android/app/src/main/java/com/example/darya/uidesignnaive/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.darya.uidesignnaive;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    var title = 'Web Images';
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: Text(title),
+        // ),
+        body: Container(
+          alignment: const Alignment(-.2, 0),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage("https://i.pinimg.com/originals/74/cb/17/74cb17188e940c3e32909d5c5d96883f.jpg"),
+                fit: BoxFit.cover)
+            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MaterialButton(
+
+                onPressed: () {},
+                color: Colors.grey[300],
+                textColor: Colors.white,
+                child: Icon(
+                  Icons.shopping_cart_outlined,
+                  size: 24,
+                ),
+                padding: EdgeInsets.all(16),
+                shape: CircleBorder(),
+              ),
+              MaterialButton(
+
+                onPressed: () {},
+                color: Colors.grey[300],
+                textColor: Colors.white,
+                child: Icon(
+                  Icons.account_balance,
+                  size: 24,
+                ),
+                padding: EdgeInsets.all(16),
+                shape: CircleBorder(),
+              ),
+              MaterialButton(
+
+                onPressed: () {},
+                color: Colors.grey[300],
+                textColor: Colors.white,
+                child: Icon(
+                  Icons.star_rate,
+                  size: 24,
+                ),
+                padding: EdgeInsets.all(16),
+                shape: CircleBorder(),
+              ),
+
+
+            ],
+          ),
+
+
+
+          ),
+
+      ),
+    );
+  }
+}
